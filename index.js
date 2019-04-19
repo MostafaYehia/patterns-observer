e7bf0c7732111d12ec4b338bb974008d46170395
MostafaYehia <mostafayehia212@gmail.com>
2019-04-19 17:17:38 +0200

    2 files modified

--- index.js
+++ index.js
@@ -6,20 +6,14 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 
-function x(ev) {
-  console.log("[Event 1] New event has been occurred", ev);
-}
-
-
-const clickEvent1 = events.subscribe('click', x);
+// clickEvent1.unsubscribe();
 
 
 const clickEvent2 = events.subscribe('click', (ev) => {
   console.log("[Event 2] New event has been occurred", ev);
 })
 
-
-clickEvent2.unsubscribe();
+// clickEvent2.unsubscribe();
 
 const clickEvent3 = events.subscribe('click', (ev) => {
   console.log("[Event 3] New event has been occurred", ev);
