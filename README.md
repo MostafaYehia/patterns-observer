894a8a9bb36a240ffaec4aec923447c35c2e2e90
MostafaYehia <mostafayehia212@gmail.com>
2019-04-19 15:10:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# patterns-observer
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/patterns-observer)
---
+++ index.js
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+
+
+require(['eventmanager'], function (eventManager) {
+
+
+
+}); 
