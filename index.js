c1bcda47208cf5150ba5df08e23361867f1ab71a
MostafaYehia <mostafayehia212@gmail.com>
2019-05-19 07:23:22 +0200

    1 file modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import './style.css';
 import events from './event-manager';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Javascript patterns - Observer</h1>`;
+appDiv.innerHTML = `<h1>Javascript patterns - PubSub</h1>`;
 
 
 // Hold a reference for the subscription
